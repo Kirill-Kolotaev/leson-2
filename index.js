509c49a768ca7e9b8ad18e6f1e64ec346d4cd80b
Kirill-Kolotaev <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>
2024-03-25 13:54:55 +0300

    leson2

---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict'
+// ЗАДАНИЕ ДОМАШНЕЕ (1)
+
+// let x = 20
+// let y = 58
+// let z = 42
+// const answer = Number(x + y + z)
+// console.log(answer)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (2)
+
+// const seconds = 60
+// const minute = 60
+// const hour = 24
+// const day = 365
+// const age = 24
+// const myAgeInSeconds = seconds * minute * hour * day * age
+// console.log(myAgeInSeconds)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (3)
+
+// const count = 42
+// const userName = '42'
+// const newCount = String(count)
+// const newUserName = Number(userName)
+// console.log(newCount, newUserName)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (4)
+
+// const a = 1
+// const b = 2
+// const c = 'белых медведей'
+// const otvet = String(a) + String(b) + ' ' + c
+// console.log(otvet)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (5)
+
+// const access = 'доступ'
+// const marine = 'морпех'
+// const ice = 'наледь'
+// const reproach = 'попрек'
+// const chopper = 'рубило'
+// const LengthWords = access + marine + ice + reproach + chopper
+// const newLengthWords = LengthWords.length
+// console.log(newLengthWords)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (6)
+const a = 24
+const b = 'Kirill'
+const c = true
+console.log(a, typeof a)
+console.log(b, typeof b)
+console.log(с, typeof с)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (7)
+
+// const a = prompt('Укажите свой возраст:')
+// const d = prompt('Укажите своё имя:')
+// console.log(a)
+// console.log(d)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (ПРОДВИНУТЫЙ УРОВЕНЬ 1)
+
+// let a = 4
+// let b = 3
+// a = a + b
+// b = a - b
+// a = a - b
+// console.log(a)
+// console.log(b)
+
+// ЗАДАНИЕ ДОМАШНЕЕ (ПРОДВИНУТЫЙ УРОВЕНЬ 2)
+
+// let codeWord1 = 'обернись'
+// let codeWord2 = 'неужели'
+// let codeWord3 = 'огурцы'
+// let codeWord4 = 'липкие'
+// let codeWord5 = '?!'
+// const cipher =
+// 	codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]
+// console.log(cipher)
